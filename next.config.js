a0f27cfe514e017eb8fbddfff556a9c7b9d349a1
Dominic Garms <djgarms@gmail.com>
2021-11-08 17:23:43 +0800

    fix layouting

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ let config = require('lumen-cms-core/config/nextjs_prod_config')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   i18n: {
     defaultLocale: 'de',
     locales: ['en', 'de'],
