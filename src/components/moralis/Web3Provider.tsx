1283817e8874961ef55c4a5a5694a515ff08889e
Dominic Garms <djgarms@gmail.com>
2021-11-25 15:53:14 +0800

    remove unused func

--- src/components/moralis/Web3Provider.tsx
+++ src/components/moralis/Web3Provider.tsx
@@ -2,17 +2,6 @@ import { FC } from 'react'
 import { Web3ReactProvider } from '@web3-react/core'
 import { ethers } from 'ethers'
 
-// import Web3 from 'web3'
-
-async function getLibrary(provider: any) {
-  // return new Eth(provider)
-  // return new ethers.providers.Web3Provider(provider)
-  // console.log('inside get Lib', provider)
-  // // @ts-ignore
-  // ContractDefault.setProvider(provider)
-  // return ContractDefault
-}
-
 const Web3Provider: FC = ({ children }) => {
   return (
     <Web3ReactProvider getLibrary={(provider) => {
