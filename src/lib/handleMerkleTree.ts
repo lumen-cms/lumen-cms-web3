c7794e480a55194ad8687d35a2bc824b5459c361
Dominic Garms <djgarms@gmail.com>
2022-01-03 18:06:10 +0800

    restructure code

---
+++ src/lib/handleMerkleTree.ts
@@ -0,0 +1,40 @@
+import keccak256 from 'keccak256'
+import { MerkleTree } from 'merkletreejs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const getMerkleRootProof = (address: string, addresses: string[]) => {
+  const leafNodes = addresses.map((address) => keccak256(address))
+  const tree = new MerkleTree(leafNodes, keccak256, { sortPairs: true })
+  return {
+    root: tree.getHexRoot(),
+    proof: tree.getHexProof(keccak256(address))
+  }
+}
+
+const getDummyProof = () => {
+  const leaf = keccak256('0x00')
+  // const hexProof = tree.getHexProof(leaf);
+  return [leaf]
+}
+
+export default async function handleMerkleTree(req: NextApiRequest, res: NextApiResponse, whitelist: string[]) {
+  const { address } = req.query
+  if (typeof address === 'string') {
+    const { root, proof } = getMerkleRootProof(address, whitelist)
+    let isWhitelisted = proof.length > 0
+    console.log(root, proof)
+    res.setHeader(
+      'Cache-Control',
+      'public, s-maxage=10, stale-while-revalidate=59'
+    )
+    res.json({
+      isWhitelisted,
+      proof: isWhitelisted ? proof : getDummyProof()
+    })
+  } else {
+    res.json({
+      isWhitelisted: false,
+      proof: getDummyProof()
+    })
+  }
+}
