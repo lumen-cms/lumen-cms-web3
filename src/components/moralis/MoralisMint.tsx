6ac88b4f74a8505ef5436c7b6381a18f9056d6eb
Dominic Garms <djgarms@gmail.com>
2022-02-16 16:41:05 +0800

    update

--- src/components/moralis/MoralisMint.tsx
+++ src/components/moralis/MoralisMint.tsx
@@ -8,15 +8,13 @@ import { useWeb3React } from '@web3-react/core'
 import { ethers } from 'ethers'
 import { CHAINS } from './chainsConfig'
 import dynamic from 'next/dynamic'
-import { getPurchaseEventData } from './eventHelper'
+import { getMintErrorMessage, getPurchaseEventData, MintError } from './eventHelper'
 
 const MoralisStripePayNow = dynamic(() => import('./MoralisStripePayNow'), {
   ssr: false
 })
 
-type MintError = {
-  message: string, code: 'insufficient_fund' | 'not_whitelisted' | 'sale_not_started' | 'max_mint_amount_exceed' | 'unknown'
-}
+
 const envAbi = process.env.NEXT_PUBLIC_ABI ? JSON.parse(process.env.NEXT_PUBLIC_ABI) : null
 export default function MoralisMint({ content }: MoralisMintProps): JSX.Element {
   const { account, chainId, library } = useWeb3React()
@@ -24,6 +22,9 @@ export default function MoralisMint({ content }: MoralisMintProps): JSX.Element 
   const amountRef = useRef<number>(1)
   const selectedChain = CHAINS[content.chain || 'mainnet']
   const isCorrectChain = selectedChain?.id === chainId
+  const currentCost = content.sale === 'whitelist'
+    ? content.price_whitelist as string
+    : content.price as string
   let mintAmount = 1
   if (content.sale === 'whitelist' && content.mint_amount_whitelist) {
     mintAmount = Number(content.mint_amount_whitelist)
@@ -43,94 +44,18 @@ export default function MoralisMint({ content }: MoralisMintProps): JSX.Element 
   const [error, setError] = useState<MintError | null>()
   const [success, setSuccess] = useState<boolean>()
 
-  const mintFunction = async () => {
-    if (content.contract_token && abi && account) {
-      const selectedAmount = amountRef.current || 1
-      const currentCost = content.sale === 'whitelist'
-        ? content.price_whitelist as string
-        : content.price as string
-
-      const value = ethers.utils.parseEther(currentCost).mul(selectedAmount)
-      const { google, facebook } = getPurchaseEventData(content, {
-        currentCost,
-        amount: selectedAmount
-      })
-
-      window.gtag &&
-      gtag('event', 'begin_checkout', google)
-      window.fbq && fbq('track', 'InitiateCheckout', facebook)
-
-      const signer = library.getSigner()
-      const merkleProof: { isWhitelisted: boolean, proof: any[] } = content.sale === 'whitelist' ? await fetch('/api/merkle/' + account)
-        .then((r) => r.json()) : {
-        isWhitelisted: false
-      }
-      if (content.sale === 'whitelist' && !merkleProof.isWhitelisted) {
-        setError({
-          message: 'You are not whitelisted. If you are make sure you have the right account connected.',
-          code: 'not_whitelisted'
-        })
-        return
-      }
-
-      const contract = new ethers.Contract(content.contract_token, abi, signer)
-      try {
-        if (process.env.NEXT_PUBLIC_MINT_CALL === 'wild') {
-          await contract.functions.mint(selectedAmount, account, {
-            value: value
-          })
-        } else {
-          await contract.functions.mint(selectedAmount, merkleProof.isWhitelisted ? merkleProof.proof : [ethers.utils.keccak256('0x00')], {
-            value: value
-          })
-        }
-        window.gtag &&
-        gtag('event', 'purchase', google)
-        window.fbq && fbq('track', 'Purchase', facebook)
-
-        setSuccess(true)
-      } catch (error: any) {
-        if (error.code === 4001) {
-          return
-        }
-        const message = error?.data?.message || error?.message
-        if (message) {
-          let currentError: MintError = {
-            message: error.message.split('(')[0],
-            code: 'unknown'
-          }
-
-          if (message?.includes('insufficient funds')) {
-            currentError = { code: 'insufficient_fund', message: 'You don\'t have enough funds in your wallet.' }
-          } else if (message?.includes('Sale has not started yet')) {
-            currentError = {
-              message: 'The sale has not started yet! Please come back later.',
-              code: 'sale_not_started'
-            }
-          } else if (message?.includes('max mint amount exceeded')) {
-            currentError = {
-              message: 'You already minted the maximum NFTs for your wallet.',
-              code: 'max_mint_amount_exceed'
-            }
-          } else if (message?.includes('invalid proof')) {
-            currentError = {
-              message: 'You are not member of the whitelist. If you are make sure you have the right account connected.',
-              code: 'not_whitelisted'
-            }
-          }
-          window.gtag &&
-          gtag('event', 'exception', {
-            event_category: 'Mint Error',
-            event_label: currentError.code
-          })
-          setError(currentError)
-        }
-      }
+  const trackEvent = (isPurchase?: boolean) => {
+    const selectedAmount = amountRef.current || 1
+    const { google, facebook } = getPurchaseEventData(content, {
+      currentCost,
+      amount: selectedAmount
+    })
+    if (isPurchase) {
+      window.fbq && fbq('track', 'Purchase', facebook)
+      window.gtag && gtag('event', 'purchase', google)
     } else {
-      setError({
-        message: 'There is something wrong with the initialization of Web3. Contact us in case the error persist.',
-        code: 'unknown'
-      })
+      window.fbq && window.fbq('track', 'InitiateCheckout')
+      window.gtag && gtag('event', 'begin_checkout')
     }
   }
 
@@ -201,8 +126,57 @@ export default function MoralisMint({ content }: MoralisMintProps): JSX.Element 
             ...content.mint_style?.[0]
           } as ButtonStoryblok}
           onClick={async () => {
-            await mintFunction()
-          }} />
+            if (content.contract_token && abi && account) {
+              const selectedAmount = amountRef.current || 1
+              const value = ethers.utils.parseEther(currentCost).mul(selectedAmount)
+              trackEvent()
+              const signer = library.getSigner()
+              const merkleProof: { isWhitelisted: boolean, proof: any[] } = content.sale === 'whitelist'
+                ? await fetch('/api/merkle/' + account)
+                  .then((r) => r.json())
+                : {
+                  isWhitelisted: false
+                }
+              if (content.sale === 'whitelist' && !merkleProof.isWhitelisted) {
+                setError({
+                  message: 'You are not whitelisted. If you are make sure you have the right account connected.',
+                  code: 'not_whitelisted'
+                })
+                return
+              }
+              const contract = new ethers.Contract(content.contract_token, abi, signer)
+              try {
+                if (process.env.NEXT_PUBLIC_MINT_CALL === 'wild') {
+                  await contract.functions.mint(selectedAmount, account, {
+                    value: value
+                  })
+                } else {
+                  await contract.functions.mint(selectedAmount, merkleProof.isWhitelisted ? merkleProof.proof : [ethers.utils.keccak256('0x00')], {
+                    value: value
+                  })
+                }
+                trackEvent(true)
+                setSuccess(true)
+              } catch (error: any) {
+                if (error.code === 4001) {
+                  return
+                }
+                const currentError = getMintErrorMessage(error)
+                window.gtag &&
+                gtag('event', 'exception', {
+                  event_category: 'Mint Error',
+                  event_label: currentError.code
+                })
+                setError(currentError)
+              }
+            } else {
+              setError({
+                message: 'There is something wrong with the initialization of Web3. Contact us in case the error persist.',
+                code: 'unknown'
+              })
+            }
+          }}
+        />
         {process.env.NEXT_PUBLIC_STRIPE_PK && content.price_fiat && (
           <MoralisStripePayNow mintAmount={() => amountRef.current}
                                contractToken={content.contract_token}
